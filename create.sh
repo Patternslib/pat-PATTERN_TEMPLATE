28661b3a9002b70a99244d95533ffc3fd3445225
Johannes Raggam <thetetet@gmail.com>
2020-11-19 16:49:56 +0100

    Add script to create project from template

---
+++ create.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [[ $# -eq 0 ]] ; then
+    echo "Usage: ./create.sh PROJECTNAME"
+    exit 1
+fi
+
+SEARCHSTRING="PATTERN_TEMPLATE"
+PROJECTNAME=$1
+
+git checkout-index -a -f --prefix=./pat-$PROJECTNAME/
+rm pat-$PROJECTNAME/create.sh
+rename "s/$SEARCHSTRING/$PROJECTNAME/" pat-$PROJECTNAME/src/*
+find ./pat-$PROJECTNAME -type f -exec sed -i "s/$SEARCHSTRING/$PROJECTNAME/g" {} \;
+cd pat-$PROJECTNAME
+git init
+git add .
+git commit -m"Initial for pat-$PROJECTNAME"
+
