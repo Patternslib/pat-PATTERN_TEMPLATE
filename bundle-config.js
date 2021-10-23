b9940ea543b21d504eee66e983e458a49e4c9043
Johannes Raggam <thetetet@gmail.com>
2021-10-23 03:28:40 +0200

    maint(build): Add Webpack module federation support.

--- bundle-config.js
+++ bundle-config.js
@@ -1,3 +1,4 @@
+import "@patternslib/patternslib/src/globals";
 import registry from "@patternslib/patternslib/src/core/registry";
 import "./src/PATTERN_TEMPLATE";
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,15 @@
 process.traceDeprecation = true;
 const path = require("path");
 const patternslib_config = require("@patternslib/patternslib/webpack/webpack.config.js");
+const package_json = require("./package.json");
 
 module.exports = async (env, argv) => {
     let config = {
         entry: {
-            "bundle.min": path.resolve(__dirname, "bundle-config.js"),
+            "bundle.min": path.resolve(__dirname, "index.js"),
         },
     };
-    config = patternslib_config(env, argv, config);
+    config = patternslib_config(env, argv, config, [], package_json.dependencies);
     config.output.path = path.resolve(__dirname, "dist/");
 
     if (process.env.NODE_ENV === "development") {
