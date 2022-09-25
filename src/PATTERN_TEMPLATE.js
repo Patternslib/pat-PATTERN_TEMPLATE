b55fc653f49c6b57414348da95ce33d2451d7c1b
Johannes Raggam <thetetet@gmail.com>
2022-09-26 01:44:01 +0200

    feat: Switch to class based Patterns.

--- src/PATTERN_TEMPLATE.js
+++ src/PATTERN_TEMPLATE.js
@@ -1,28 +1,36 @@
-import Base from "@patternslib/patternslib/src/core/base";
+import { BasePattern } from "@patternslib/patternslib/src/core/basepattern";
 import Parser from "@patternslib/patternslib/src/core/parser";
+import registry from "@patternslib/patternslib/src/core/registry";
 
 export const parser = new Parser("PATTERN_TEMPLATE");
-parser.addArgument("example-option", [1, 2, 3]);
+parser.addArgument("example-option", "Stranger");
 
-export default Base.extend({
-    name: "PATTERN_TEMPLATE",
-    trigger: ".pat-PATTERN_TEMPLATE",
+class Pattern extends BasePattern {
+    static name = "PATTERN_TEMPLATE";
+    static trigger = ".pat-PATTERN_TEMPLATE";
+    parser = parser;
 
     async init() {
         import("./PATTERN_TEMPLATE.scss");
 
-        this.options = parser.parse(this.el, this.options);
-
-        // Just an example!
+        // Try to avoid jQuery, but here is how to import it.
         // eslint-disable-next-line no-unused-vars
-        const $ = (await import("jquery")).default; // try to avoid jQuery.
+        const $ = (await import("jquery")).default;
 
-        // Just an example!
-        // eslint-disable-next-line no-unused-vars
+        // The options are automatically created, if parser is defined.
         const example_option = this.options.exampleOption;
+        this.el.innerHTML = `
+            <p>hello ${example_option}, this is pattern ${this.name} speaking.</p>
+        `;
+    }
+}
 
-        // Just an example!
-        // And completly useless.
-        this.el.innerHTML = "<p>hello.</p>";
-    },
-});
+// Register Pattern class in the global pattern registry and make it usable there.
+registry.register(Pattern);
+
+// Export Pattern as default export.
+// You can import it as ``import AnyName from "./PATTERN_TEMPLATE";``
+export default Pattern;
+// Export BasePattern as named export.
+// You can import it as ``import { Pattern } from "./PATTERN_TEMPLATE";``
+export { Pattern };
--- src/PATTERN_TEMPLATE.test.js
+++ src/PATTERN_TEMPLATE.test.js
@@ -10,11 +10,25 @@ describe("pat-PATTERN_TEMPLATE", () => {
         document.body.innerHTML = `<div class="pat-PATTERN_TEMPLATE" />`;
         const el = document.querySelector(".pat-PATTERN_TEMPLATE");
 
-        // Just an example!
-        // eslint-disable-next-line no-unused-vars
         const instance = new Pattern(el);
         await utils.timeout(1); // wait a tick for async to settle.
 
-        expect(el.innerHTML).toBe("<p>hello.</p>");
+        expect(el.innerHTML.trim()).toBe(
+            `<p>hello ${instance.options.exampleOption}, this is pattern ${instance.name} speaking.</p>`
+        );
+    });
+    it("is initialized correctly with options from attribute", async () => {
+        document.body.innerHTML = `<div
+            class="pat-PATTERN_TEMPLATE"
+            data-pat-PATTERN_TEMPLATE='{"example-option": "World"}'
+            />`;
+        const el = document.querySelector(".pat-PATTERN_TEMPLATE");
+
+        const instance = new Pattern(el);
+        await utils.timeout(1); // wait a tick for async to settle.
+
+        expect(el.innerHTML.trim()).toBe(
+            `<p>hello World, this is pattern ${instance.name} speaking.</p>`
+        );
     });
 });
