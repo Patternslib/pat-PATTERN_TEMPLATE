d11464e762d9374519e180ca5a7eb700fc9a5a93
Johannes Raggam <thetetet@gmail.com>
2026-09-21 14:34:49 +0200

    feat: Allow await bundle registration.
    Allow the module federation loader to await the registration of the
    Patterns in this bundle.

--- index.js
+++ index.js
@@ -1,5 +1,2 @@
 // Webpack entry point for module federation.
-
-// This import needs to be kept with brackets, otherwise we get this error:
-// "Shared module is not available for eager consumption."
-import("./bundle");
+export default import("./bundle");
